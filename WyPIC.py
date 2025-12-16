97c33d3f6ec8ebe370e771dc31e817b9775fc7e6
WyLax <artemarabik@gmail.com>
2025-12-16 21:36:00 +0500

    Update WyPIC.py

--- WyPIC.py
+++ WyPIC.py
@@ -236,7 +236,7 @@ async def handle_message(message: types.Message):
 # -----------------------------
 
 async def main():
-    print("Бот запущен...")
+    print("Бот запущен....")
     await dp.start_polling(bot)
 
 
